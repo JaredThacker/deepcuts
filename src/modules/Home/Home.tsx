bdada7f371c20de165b78a6ea624bfb37da1d705
jaydeetee97 <pricesmayvary302@gmail.com>
2024-08-23 22:26:31 -0400

    added route for getting records, added synthwave theme

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -8,19 +8,11 @@ export const Home = () => {
     const [record, setRecord] = React.useState<DiscogsRecord>();
 
     const getRecord = async () => {
-        let responseStatus = 404;
-        let parsedRecord: DiscogsRecord | undefined = undefined;
-        while (responseStatus === 404) {
-            const id = Math.floor(Math.random() * 31105274);
-            const response = await fetch(
-                "https://api.discogs.com/releases/" +
-                    id +
-                    "?token=pnVhiBkGbDrHnRDrPMCKfDBXwHhvRdfLBvRgvzKb",
-            );
-            const jsonResponse = await response.json();
-            parsedRecord = jsonResponse as DiscogsRecord;
-            responseStatus = response.status;
-        }
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/record`,
+        );
+        const parsedResponse = await response.json();
+        const parsedRecord = parsedResponse as DiscogsRecord;
         setRecord(parsedRecord);
     };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,23 +2,26 @@ import type { Config } from "tailwindcss";
 import { withAnimations } from "animated-tailwindcss";
 
 const config: Config = withAnimations({
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+
+    daisyui: {
+        themes: ["light", "dark", "synthwave"],
     },
-  },
-  plugins: [
-    require("daisyui")
-  ],
+
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
+    },
+    plugins: [require("daisyui")],
 }) as any as Config;
 export default config;
