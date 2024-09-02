1542b1cbceacc697bffee78dd51992006586f830
jaydeetee97 <pricesmayvary302@gmail.com>
2024-09-02 13:23:42 -0400

    Added the timestamps for all tables and added it to the user creation flow

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,30 +1,16 @@
 "use client";
 
-import { DiscogsRecord } from "@/types/DiscogsRecord";
 import React from "react";
-import { RecordDisplay } from "../RecordDisplay/RecordDisplay";
 
 export const Home = () => {
-    const [record, setRecord] = React.useState<DiscogsRecord>();
-
-    const getRecord = async () => {
-        const response = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/record`,
-        );
-        const parsedResponse = await response.json();
-        const parsedRecord = parsedResponse as DiscogsRecord;
-        setRecord(parsedRecord);
-    };
-
     return (
-        <div className="h-full flex flex-col justify-center items-center">
-            {record !== undefined && <RecordDisplay record={record} />}
-            <button
-                className="btn btn-active btn-neutral w-fit focus:animate-headShake"
-                onClick={getRecord}
-            >
-                {"Randomize!"}
-            </button>
+        <div className="grow flex flex-row p-4 gap-4">
+            <div className="h-full border grow border-gray-500 rounded">
+                {"Random History"}
+            </div>
+            <div className="h-full border grow border-green-500 rounded">
+                {"There"}
+            </div>
         </div>
     );
 };
