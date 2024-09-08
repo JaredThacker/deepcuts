d67dff6670b3edb861ac9949d398301cbf3de0d5
jaydeetee97 <pricesmayvary302@gmail.com>
2024-09-08 16:08:39 -0400

    protected pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,17 @@
+import { headers } from "@/common/constants/headers";
 import { Home } from "@/modules/Home/Home";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default Home;
+const Dashboard = () => {
+    const session = cookies().get(headers.SESSION);
+
+    if (session === undefined) {
+        redirect("/");
+        return <span />;
+    }
+
+    return <Home />;
+};
+
+export default Dashboard;
--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -1,3 +1,17 @@
+import { headers } from "@/common/constants/headers";
 import { Random } from "@/modules/Random/Random";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default Random;
+const RandomPage = () => {
+    const session = cookies().get(headers.SESSION);
+
+    if (session === undefined) {
+        redirect("/");
+        return <span />;
+    }
+
+    return <Random />;
+};
+
+export default RandomPage;
