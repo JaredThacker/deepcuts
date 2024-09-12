deb4a12df40b598ced55525604b88b3870e5be61
jaydeetee97 <pricesmayvary302@gmail.com>
2024-09-11 20:11:02 -0400

    outline album cover on hover

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -11,7 +11,7 @@ export const Home = () => {
         <div className="grow flex flex-row p-4 gap-4 animate-fadeIn">
             <div className="h-full border grow border-gray-500 rounded">
                 <div className="h-fit w-11/12 m-8 flex flex-col items-center border grow border-blue-200 rounded">
-                    <span className="flex flex-col items-center underline font-bold">
+                    <span className="flex flex-col items-center font-bold mb-1">
                         Record of the Day
                     </span>
 
@@ -24,10 +24,11 @@ export const Home = () => {
                                 src="https://i.discogs.com/mcIh1A6HEAVQ0pCwZ1mnLqxREjgwikqQijZafZMZ5uI/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQzMTIy/NzMtMTM2MTUyOTY1/OS0zMzUzLmpwZWc.jpeg"
                                 width={300}
                                 height={300}
+                                className="hover:outline outline-primary"
                             ></img>
                         </a>
                     </div>
-                    <span className="flex flex-col items-center">
+                    <span className="flex flex-col items-center mt-1">
                         Atoms For Peace - Amok (2013){" "}
                         <span>Genre: Electronic, Rock</span>
                         <span>
