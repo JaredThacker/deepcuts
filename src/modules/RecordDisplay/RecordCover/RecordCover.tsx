5fabd91b99bc289221b14649dfa1cc455b9ffe2c
jaydeetee97 <pricesmayvary302@gmail.com>
2024-09-11 22:30:13 -0400

    cursor pointer on record cover

--- src/modules/RecordDisplay/RecordCover/RecordCover.tsx
+++ src/modules/RecordDisplay/RecordCover/RecordCover.tsx
@@ -11,7 +11,7 @@ export const RecordCover = (props: RecordCoverProperties) => (
     <img
         alt="Default Record Image"
         id="RecordCover"
-        className="h-50 w-50"
+        className="h-50 w-50 hover:cursor-pointer"
         height={300}
         width={300}
         src={props.src === undefined ? props.image?.uri : props.src}
