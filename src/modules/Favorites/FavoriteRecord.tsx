0592aee65d394c05e51df34cf76e005c7ab0c520
jaydeetee97 <pricesmayvary302@gmail.com>
2024-10-02 22:01:47 -0400

    swapped anchor tags with double click

--- src/modules/Favorites/FavoriteRecord.tsx
+++ src/modules/Favorites/FavoriteRecord.tsx
@@ -9,18 +9,20 @@ type UserFavoriteProperties = {
 export const FavoriteRecord = ({
     favoriteRecord,
 }: UserFavoriteProperties): JSX.Element => (
-    <a
-        href={`https://discogs.com/release/${favoriteRecord.recordid}`}
-        rel="noreferrer"
-        target="_blank"
-    >
-        <Image
-            className="hover:outline outline-gray-400 rounded-full h-14 w-14 hover:cursor-pointer transition-all hover:animate-twSpin hover:animate-infinite hover:animate-duration-[1760ms]"
-            alt=""
-            height={150}
-            width={150}
-            src={favoriteRecord.image_uri ?? placeholderImageSrc}
-            loading="lazy"
-        />
-    </a>
+    <Image
+        className="hover:outline outline-gray-400 rounded-full h-14 w-14 hover:cursor-pointer transition-all hover:animate-twSpin hover:animate-infinite hover:animate-duration-[1760ms]"
+        alt=""
+        height={150}
+        width={150}
+        src={favoriteRecord.image_uri ?? placeholderImageSrc}
+        onClick={() => {}}
+        onDoubleClick={() => {
+            window.open(
+                `https://discogs.com/release/${favoriteRecord.recordid}`,
+                "_blank",
+                "noreferrer",
+            );
+        }}
+        loading="lazy"
+    />
 );
--- src/modules/UserHistory/UserHistoryRecord.tsx
+++ src/modules/UserHistory/UserHistoryRecord.tsx
@@ -8,18 +8,20 @@ type UserHistoryRecordProperties = {
 export const UserHistoryRecord = ({
     historyRecord,
 }: UserHistoryRecordProperties): JSX.Element => (
-    <a
-        href={`https://discogs.com/release/${historyRecord.recordid}`}
-        rel="noreferrer"
-        target="_blank"
-    >
-        <Image
-            className="hover:outline outline-gray-400 rounded-full h-14 w-14 hover:cursor-pointer transition-all hover:animate-twSpin hover:animate-infinite hover:animate-duration-[1760ms]"
-            alt=""
-            height={150}
-            width={150}
-            src={historyRecord.image_uri}
-            loading="lazy"
-        />
-    </a>
+    <Image
+        className="hover:outline outline-gray-400 rounded-full h-14 w-14 hover:cursor-pointer transition-all hover:animate-twSpin hover:animate-infinite hover:animate-duration-[1760ms]"
+        alt=""
+        height={150}
+        width={150}
+        src={historyRecord.image_uri}
+        onClick={() => {}}
+        onDoubleClick={() => {
+            window.open(
+                `https://discogs.com/release/${historyRecord.recordid}`,
+                "_blank",
+                "noreferrer",
+            );
+        }}
+        loading="lazy"
+    />
 );
