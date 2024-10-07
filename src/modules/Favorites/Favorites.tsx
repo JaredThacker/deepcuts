7e09fec0aec51004da8ba911493dea008c427e74
jaydeetee97 <pricesmayvary302@gmail.com>
2024-10-07 18:33:57 -0400

    bg-color for history and favorites

--- src/modules/Favorites/Favorites.tsx
+++ src/modules/Favorites/Favorites.tsx
@@ -13,7 +13,7 @@ export const Favorites = () => {
     }
 
     return (
-        <div className="flex flex-row gap-2 flex-wrap pt-2 pl-2 pb-2 border border-accent border-dotted border-opacity-45">
+        <div className="flex flex-row gap-2 flex-wrap pt-2 pl-2 pb-2 border border-dotted border-accent border-opacity-45 bg-slate-800 bg-opacity-45">
             {favoritesData.map((eachFavorite) => (
                 <FavoriteRecord
                     key={eachFavorite.recordid}
