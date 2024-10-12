3d2c5e5c8095a037cf7bbde1015706b5c6ee02f0
jaydeetee97 <pricesmayvary302@gmail.com>
2024-10-11 20:28:14 -0400

    put border back and opacity

--- src/modules/UserHistory/UserHistory.tsx
+++ src/modules/UserHistory/UserHistory.tsx
@@ -8,7 +8,7 @@ type HistoryProperties = {
 };
 
 export const UserHistory = ({ history }: HistoryProperties): JSX.Element => (
-    <div className="flex flex-row items-center justify-between w-full p-4 gap-5 bg-slate-800 bg-opacity-45">
+    <div className="flex flex-row items-center justify-between w-full p-4 gap-5 border border-dotted border-accent border-opacity-45 bg-slate-800 bg-opacity-30">
         <span className="text-lg font-bold">{`${dayjs(
             history.created_at,
         ).format("YYYY-MM-DD hh:mm")}`}</span>
