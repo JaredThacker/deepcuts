72ea16a3172c34e0b402cc0bac6fd3e72d2d91a4
jaydeetee97 <pricesmayvary302@gmail.com>
2025-03-07 00:55:25 -0500

    FAQ

--- src/modules/Support/Support.tsx
+++ src/modules/Support/Support.tsx
@@ -8,9 +8,31 @@ export const Support = () => {
 
     return (
         <div className="flex flex-col h-full w-full items-center justify-center gap-7">
-            <p>
+            <div className="flex flex-col justify-center text-wrap h-64 w-2/3 animate-fadeIn animate-duration-[2000ms]">
+                <h1 className="text-3xl">{"FAQ:"}</h1>
+                <p className="text-info whitespace-pre-line">
+                    {
+                        "\nQ: When I randomize, why does my amount remaining go down by more than one sometimes?\n"
+                    }
+                </p>
+                <p className="whitespace-pre-line">
+                    {
+                        "\nA: Due to the nature of the algorithm fetching these random releases from the Discogs API, sometimes it lands on a release number that does not exist. When that happens, it will re-roll but the API request still counts towards your rate limit."
+                    }
+                </p>
+                <p className="text-info whitespace-pre-line">
+                    {"\nQ: What is a rate limit?"}
+                </p>
+                <p className="whitespace-pre-line">
+                    {
+                        "\nA: The rate limit is the amount of requests your IP address can make to the Discogs API per rolling minute. You will notice when you make a request without filters it will start around 60, and if you make one with filters it will start around 25. As far as I can tell, these limits are independent of each other and using one does not affect the other. Authenticated users should have 60 unfiltered requests and 25 filtered requests per rolling minute!"
+                    }
+                </p>
+            </div>
+
+            <p className="whitespace-pre-line animate-fadeIn animate-duration-[2000ms]">
                 {
-                    "All support inquiries or questions you may have please contact me at \n"
+                    "\nAll support inquiries, questions, or feature requests you may have please contact me at"
                 }
                 <Link
                     target="_blank"
@@ -20,7 +42,7 @@ export const Support = () => {
                     <span className="">{supportEmail}</span>
                 </Link>
             </p>
-            <div className="w-52">
+            <div className="w-52 animate-fadeIn animate-duration-[2000ms]">
                 <a
                     href="https://www.buymeacoffee.com/deepcutsofficial"
                     target="_blank"
